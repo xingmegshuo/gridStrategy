dc982bf15df920ba99aa207cf1adc91873c09758
small_ant <xms.chnb@gmail.com>
2021-07-14 19:19:34 +0800

    v1

--- models/user.go
+++ models/user.go
@@ -80,6 +80,7 @@ func NewUser() {
 				result := DB.Where(&User{ObjectId: int32(order["id"].(float64))}).First(&u)
 				// 			// 条件 数据库未找到，订单启用，创建新的任务
 				if errors.Is(result.Error, gorm.ErrRecordNotFound) && order["status"].(float64) == 1 {
+					log.Println(NewApi[order["customer_id"]])
 					u = User{
 						ObjectId: int32(order["id"].(float64)),
 						ApiKey:   NewApi[order["customer_id"]][order["category_id"]]["apikey"].(string),

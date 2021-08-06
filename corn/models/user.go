4cf97ffbdf2da768b17e0d3d2ecfd726ab3a9ac9
small_ant <xms.chnb@gmail.com>
2021-08-06 15:47:18 +0800

    update

--- corn/models/user.go
+++ corn/models/user.go
@@ -11,6 +11,7 @@ package model
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"strings"
 	"time"
 
@@ -69,7 +70,7 @@ func NewUser() {
 			// 数据库查找存在与否
 			result := DB.Where(&User{ObjectId: int32(order["id"].(float64))}).First(&u)
 			// 条件 数据库未找到，订单启用，创建新的任务
-			// fmt.Println("hhhhh")
+			fmt.Println("查找数据")
 			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 				u = User{
 					ObjectId: int32(order["id"].(float64)),

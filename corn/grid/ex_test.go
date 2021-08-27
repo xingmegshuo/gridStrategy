bf047db6af42f82bf52adbedb0bf2e6bd52fe477
small_ant <xms.chnb@gmail.com>
2021-08-27 10:54:22 +0800

    update

--- corn/grid/ex_test.go
+++ corn/grid/ex_test.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"testing"
 	model "zmyjobs/corn/models"
-	"zmyjobs/goex"
+
+	"github.com/shopspring/decimal"
 	// "github.com/nntaoli-project/goex"
 )
 
@@ -179,21 +180,21 @@ func TestFutureAccount(t *testing.T) {
 	// fmt.Println(bian)
 	cli := NewEx(&bian)
 
-	b := goex.NewCurrencyPair2(bian.Symbol)
+	// b := goex.NewCurrencyPair2(bian.Symbol)
 	// fmt.Println(b.String())
 	// cli.Future.ChangeLever(b, goex.SWAP_CONTRACT)
 	// fmt.Println(cli.Currency)
 	// cl := util.Config{Name: "币安"}
 	// p, err := cl.GetPrice("ETH/USD", true)
 	// fmt.Println(p, err)
-	p, err := cli.Future.GetFuturePosition(b, goex.SWAP_USDT_CONTRACT)
+	// p, err := cli.Future.GetFuturePosition(b, goex.SWAP_USDT_CONTRACT)
 
 	// p, err := cli.Future.GetFuturePosition(b, goex.SWAP_CONTRACT)
-	fmt.Println(fmt.Sprintf("%+v", p), err)
+	// fmt.Println(fmt.Sprintf("%+v", p), err)
 	// o, err := cli.Future.MarketFuturesOrder(b, goex.SWAP_CONTRACT, "1", 1)
 	// fmt.Println(o, err)
-	// order, result := cli.Exchanges(decimal.NewFromFloat(0.003), decimal.Decimal{}, OpenDL, false)
-	// fmt.Println(order, result)
+	order, result := cli.Exchanges(decimal.NewFromFloat(0.003), decimal.Decimal{}, OpenDL, false)
+	fmt.Println(order, result)
 	// orderId := "8389765505681314816"
 	// b, r, o := cli.SearchOrder(orderId)
 	// fmt.Println(fmt.Sprintf("%+v", o), b, r)

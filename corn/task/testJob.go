883004b8c25a509e40ee0600aca5c825075da153
small_ant <xms.chnb@gmail.com>
2021-08-31 11:36:30 +0800

    update

--- corn/task/testJob.go
+++ corn/task/testJob.go
@@ -54,11 +54,15 @@ func CrawRun() {
 	go xhttp("https://dapi.binance.com/dapi/v1/ticker/24hr", "ZMYCOINF")
 	go xhttp("https://fapi.binance.com/fapi/v1/ticker/24hr", "ZMYUSDF")
 	go crawAccount()
-	fmt.Println("结束")
-	if time.Since(start) > time.Second*10 {
-		fmt.Println("超时退出", time.Since(start))
-		runtime.Goexit()
+	for {
+		if time.Since(start) > time.Second*10 {
+			fmt.Println("超时退出", time.Since(start))
+			runtime.Goexit()
+		} else {
+			time.Sleep(time.Second)
+		}
 	}
+
 }
 
 // xhttp 缓存信息

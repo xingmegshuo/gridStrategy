a27ea9c3963b94fc1d0b8c0b884b687af0c05b9e
small_ant <xms.chnb@gmail.com>
2021-08-31 11:39:23 +0800

    update

--- corn/task/testJob.go
+++ corn/task/testJob.go
@@ -105,14 +105,14 @@ func xhttp(url string, name string) {
 
 // craw
 func craw(coinCache []*redis.Z) {
-	// start := time.Now()
+	start := time.Now()
 	model.UserDB.Raw("select count(*) from db_task_coin").Scan(&coinCount)
 	coinCache = append(coinCache, xhttpCraw("https://api.huobi.pro/market/tickers", 1, 0)...)
 	coinCache = append(coinCache, xhttpCraw("https://api.binance.com/api/v3/ticker/24hr", 2, 0)...)
 	coinCache = append(coinCache, xhttpCraw("https://www.okex.com/api/spot/v3/instruments/ticker", 5, 0)...)
 	coinCache = append(coinCache, xhttpCraw("https://dpi.binance.com/dapi/v1/ticker/24hr", 2, 2)...)
 	coinCache = append(coinCache, xhttpCraw("https://fpi.binance.com/fapi/v1/ticker/24hr", 2, 2)...)
-	// fmt.Println(len(coinCache), coinCount, time.Since(start))
+	fmt.Println(len(coinCache), coinCount, time.Since(start))
 	if len(coinCache) == coinCount {
 		fmt.Println("write coins db")
 		model.Del("ZMYCOINS")
@@ -200,7 +200,7 @@ func WriteDB(realData []map[string]interface{}, category int, coinType int) (coi
 			}
 		}
 	}
-	fmt.Println(coinCache)
+	// fmt.Println(coinCache)
 	return
 }
 

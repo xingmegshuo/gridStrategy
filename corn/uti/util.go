35f7f8e8bbc85e9e5fdeedfca91dac9ed24648e8
small_ant <xms.chnb@gmail.com>
2021-10-09 13:42:55 +0800

    update

--- corn/uti/util.go
+++ corn/uti/util.go
@@ -9,19 +9,19 @@
 package util
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strings"
-	"time"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "strings"
+    "time"
 
-	"zmyjobs/goex"
-	"zmyjobs/goex/builder"
+    "zmyjobs/goex"
+    "zmyjobs/goex/builder"
 
-	"github.com/shopspring/decimal"
-	"golang.org/x/net/proxy"
+    "github.com/shopspring/decimal"
+    "golang.org/x/net/proxy"
 )
 
 // Config 创建client需要的配置Struct
@@ -144,6 +144,7 @@ func SwitchCoinType(name string) int {
         return 3
     }
     if name[len(name)-4:] == "usd" || name[len(name)-4:] == "USD" {
+        fmt.Println(name[len(name)-4:])
         return 2
     }
     if strings.Contains(name, "USD") || strings.Contains(name, "usd") {
